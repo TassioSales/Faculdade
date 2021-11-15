68cdbe14405d21eb9996801c8981917285e4ab44
TassioSales <tassio.ljs@gmail.com>
2021-11-15 17:21:20 -0300

    Update main.py

--- Estrutura de dados/projetos_estruturas/Caixeiro_Viajante/CaixeiroViajante/main.py
+++ Estrutura de dados/projetos_estruturas/Caixeiro_Viajante/CaixeiroViajante/main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import time
 from CaixeiroViajante.AlgoritimoGenetico import AlgoritimoGenetico
 
 quantidade = int(input("Digite a quantidade de cidades (+ que duas): "))
@@ -16,9 +17,13 @@ while geracoes < 1:
     print("Quantidade invalida!!!")
     geracoes = int(input("Digite a quantidade de gerações (+ que uma): "))
 
+inicio = time.time()
 ag = AlgoritimoGenetico(tamanhoPopulacao, quantidade, geracoes)
 teste = ag.resolver()
 print("Resultado: ", teste[0])
 plt.plot(ag.melhores)
 plt.title("Acompanhamento dos valores.\n Valor em Km, quanto menor melhor")
 plt.show()
+fim = time.time()
+total_time = fim - inicio
+print(f"O tempo de excução do Caixeiro Viajante foi foi {total_time:.2f}")
