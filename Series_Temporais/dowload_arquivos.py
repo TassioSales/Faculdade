d72172d044151c55391b381d52626c09afb18a3f
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-12-15 16:38:40 -0300

    atualizaçoes

--- Series_Temporais/dowload_arquivos.py
+++ Series_Temporais/dowload_arquivos.py
@@ -3,6 +3,7 @@ import os
 from time import sleep
 
 
+# crando lista com os nomes dos arquivos
 lista_fim_links = ['ca-2004-01.csv', 'ca-2004-02.csv', 'ca-2005-01.csv', 'ca-2005-02.csv', 'ca-2006-01.csv',
                    'ca-2006-02.csv', 'ca-2007-01.csv', 'ca-2007-02.csv', 'ca-2008-01.csv', 'ca-2008-02.csv',
                    'ca-2009-01.csv', 'ca-2009-02.csv', 'ca-2010-01.csv', 'ca-2010-02.csv', 'ca-2011-01.csv',
@@ -12,13 +13,13 @@ lista_fim_links = ['ca-2004-01.csv', 'ca-2004-02.csv', 'ca-2005-01.csv', 'ca-200
                    'ca-2019-01.csv', 'ca-2019-02.csv', 'ca-2020-01.csv', 'ca-2020-02.csv', 'ca-2021-01.csv',
                    'ca-2021-02.csv']
 
-
+#Criando função para baixar os arquivos
 def baixar_csv(item):
     link = f'https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/dsas/ca/{item}'
     # baixando o arquivo csv se ele nao existir na pasta
     #salvar os arquivos em arquivos_csv
     #mostrando o tamanho do arquivo e o tempo de download
-    if not os.path.exists(f'arquivos_csv/{item}'):
+    if not os.path.exists(f'arquivoscsv/{item}'):
         print(f'baixando o arquivo {item}')
         r = requests.get(link)
         #mostar o tamanho do arquivo em MB
@@ -27,9 +28,26 @@ def baixar_csv(item):
         print(f'tempo de download: {r.elapsed.total_seconds()} segundos')
         sleep(5)
         print('Arquivo salvo com sucesso!')
-        with open(f'arquivos_csv/{item}', 'wb') as f:
+        with open(f'arquivoscsv/{item}', 'wb') as f:
             f.write(r.content)
     else:
         print(f'arquivo {item} ja existe')
+        
+        
+#criar função para verificar se as colunas de cada arquivo são iguais
+def verificar_colunas():
+    import pandas as pd
+    #verificar colunas de uma dos arquivos csv da pasta arquivos_csv
+    df = pd.read_csv('arquivos_csv/ca-2004-01.csv', sep=';', encoding='latin-1')
+    #criar lista com as colunas do arquivo
+    coluna =[col for col in df.columns]
+    #ler o reestante dos arquivos usando a biblioteca glob e verificar se as colunas são iguais
+    for arquivo in gb.glob('arquivoscsv/*.csv'):
+        df = pd.read_csv(arquivo, sep=';', encoding='latin-1')
+        coluna2 = [col for col in df.columns]
+        if coluna == coluna2:
+            print(f'Colunas do arquivo {arquivo} são iguais')
+        else:
+            print(f'Colunas do arquivo {arquivo} são diferentes')
 
 
